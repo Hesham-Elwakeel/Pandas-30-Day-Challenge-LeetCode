30e007f391347669b8bc19d4777448a63ac83505
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-09-02 13:17:50 -0700

    Update 1148. Article Views I.py

--- Data Filtering/1148. Article Views I.py
+++ Data Filtering/1148. Article Views I.py
@@ -39,3 +39,13 @@ Output:
 | 4    |
 | 7    |
 +------+
+
+
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    df=views[views['author_id']==views['viewer_id']]
+    udf=df['author_id'].unique()
+    udf=sorted(udf)
+    result=pd.DataFrame({'id': udf})
+    return result
