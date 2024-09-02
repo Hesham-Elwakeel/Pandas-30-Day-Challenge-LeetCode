9a6fc3f24a1ac32e8f9b7fe86a87d9263dfc5f8e
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-09-02 04:41:12 -0700

    Update 1757. Recyclable and Low Fat Products.py

--- Data Filtering/1757. Recyclable and Low Fat Products.py
+++ Data Filtering/1757. Recyclable and Low Fat Products.py
@@ -43,4 +43,4 @@ def find_products(products: pd.DataFrame):
     result = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y' )]
     return result[['product_id']]
 
-**Hesham**
+1. **Hesham**
