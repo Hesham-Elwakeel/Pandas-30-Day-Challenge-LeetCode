363fa41ec8561cf02ffdfb17b4eb97df010bfa8b
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-09-02 13:17:59 -0700

    Update 1148. Article Views I.py

--- Data Filtering/1148. Article Views I.py
+++ Data Filtering/1148. Article Views I.py
@@ -11,7 +11,6 @@ There is no primary key (column with unique values) for this table, the table ma
 Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
 Note that equal author_id and viewer_id indicate the same person.
  
-
 Write a solution to find all the authors that viewed at least one of their own articles.
 Return the result table sorted by id in ascending order.
 
